ef6c9df5fbe90b4e2d9754c5c1621fbbbcc13479
Rcong <zcr0903@gmail.com>
2023-04-09 20:39:41 +0800

    some little change

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { RouterProvider } from 'react-router-dom'
 import { router } from './routes/router'
 import './app.scss'
 
-console.log('[App.tsx]', `Hello world from Electron ${process.versions.electron}!`)
+// console.log('[App.tsx]', `Hello world from Electron ${process.versions.electron}!`)
 
 export const App: React.FC = () => {
   return (
